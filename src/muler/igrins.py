68496a808dd0d95c2174d935bf458e3002e51e97
gully <igully@gmail.com>
2021-01-11 15:52:33 -0600

    Highly experimental smoothing function! based on celerite2

--- src/muler/igrins.py
+++ src/muler/igrins.py
@@ -14,8 +14,10 @@ import numpy as np
 from astropy.io import fits
 from astropy import units as u
 from astropy.nddata import StdDevUncertainty
+from celerite2 import terms
+import celerite2
+from scipy.optimize import minimize
 import os
-import copy
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore")
@@ -76,4 +78,39 @@ class IGRINSSpectrum(Spectrum1D):
             flux=self.flux[~self.mask],
             mask=self.mask[~self.mask],
             uncertainty=masked_unc,
+        )
+
+    def smooth_spectrum(self):
+        """Smooth the spectrum using Gaussian Process regression"""
+        if self.uncertainty is not None:
+            unc = self.uncertainty.array
+        else:
+            unc = np.zeros_like(self.flux.value)
+
+        kernel = terms.SHOTerm(sigma=0.03, rho=15.0, Q=0.5)
+        gp = celerite2.GaussianProcess(kernel, mean=0.0)
+        gp.compute(self.wavelength)
+
+        # Construct the GP model with celerite
+        def set_params(params, gp):
+            gp.mean = params[0]
+            theta = np.exp(params[1:])
+            gp.kernel = terms.SHOTerm(sigma=theta[0], rho=theta[1], Q=0.5)
+            gp.compute(self.wavelength.value, yerr=unc + theta[2], quiet=True)
+            return gp
+
+        def neg_log_like(params, gp):
+            gp = set_params(params, gp)
+            return -gp.log_likelihood(self.flux.value)
+
+        initial_params = [np.log(1), np.log(0.001), np.log(5.0), np.log(0.01)]
+        soln = minimize(neg_log_like, initial_params, method="L-BFGS-B", args=(gp,))
+        opt_gp = set_params(soln.x, gp)
+
+        mean_model = opt_gp.predict(self.flux.value, t=self.wavelength.value)
+
+        return IGRINSSpectrum(
+            spectral_axis=self.wavelength,
+            flux=mean_model * self.flux.unit,
+            mask=np.zeros_like(mean_model, dtype=np.bool),
         )
