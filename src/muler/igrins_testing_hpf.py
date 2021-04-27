e36a05d900af2ed66cb2e519feb53f4c22903853
JessicaLuna <jluna@astro.as.utexas.edu>
2021-04-27 14:39:47 -0500

    add support for HPF: address issue #2

---
+++ src/muler/igrins_testing_hpf.py
@@ -0,0 +1,482 @@
+r"""
+IGRINS Spectrum
+---------------
+
+A container for an IGRINS spectrum of :math:`M=28` total total orders :math:`m`, each with vectors for wavelength flux and uncertainty, e.g. :math:`F_m(\lambda)`.
+
+
+IGRINSSpectrum
+##############
+"""
+
+import warnings
+import numpy as np
+import astropy
+from astropy.io import fits
+from astropy import units as u
+from astropy.wcs import WCS, FITSFixedWarning
+from astropy.nddata import StdDevUncertainty
+from scipy.stats import median_abs_deviation
+import h5py
+from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.interpolate import UnivariateSpline
+
+
+# from specutils.io.registers import data_loader
+from celerite2 import terms
+import celerite2
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import os
+import copy
+
+#  See Issue: https://github.com/astropy/specutils/issues/779
+warnings.filterwarnings(
+    "ignore", category=astropy.utils.exceptions.AstropyDeprecationWarning
+)
+warnings.filterwarnings("ignore", category=FITSFixedWarning)
+# See Issue: https://github.com/astropy/specutils/issues/800
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore")
+    from specutils import Spectrum1D
+    from specutils import SpectrumList
+
+# Convert PLP index number to echelle order m
+## Note that these technically depend on grating temperature
+## For typical operating temperature, offsets should be exact.
+grating_order_offsets = {"H": 98, "K": 71, 'Goldilocks':0, 'Slope':0}
+
+
+class IGRINSSpectrum(Spectrum1D):
+    r"""
+    A container for IGRINS spectra
+
+    Args:
+        file (str): A path to a reduced IGRINS spectrum from plp
+        order (int): which spectral order to read
+        cached_hdus (list) :
+            List of two fits HDUs, one for the spec_a0v.fits, and one for the
+            sn.fits file, to reduce file I/O for multiorder access.
+            If provided, must give both HDUs.  Optional, default is None.
+    """
+
+    def __init__(self, *args, file=None, order=19, cached_hdus=None,sky=False, **kwargs):
+
+        if file is not None:
+            if "Goldilocks" in file:
+                band = "Goldilocks"
+            elif "Slope" in file:    
+                band = 'HPF'         
+            else:
+                raise NameError("Cannot identify file as an HPF spectrum")
+            grating_order = grating_order_offsets[band] + order
+
+
+            hdus = fits.open(str(file))
+
+            hdr = hdus[0].header
+            if sky==True:
+                # lamb = hdus[9].data[order].astype(np.float64) * u.AA #u.micron for igrins
+                # flux = hdus[3].data[order].astype(np.float64) * u.ct
+                # unc = hdus[6].data[order].astype(np.float64) * u.ct
+                lamb = hdus[8].data[order].astype(np.float64) * u.AA 
+                flux = hdus[2].data[order].astype(np.float64) * u.ct
+                unc = hdus[5].data[order].astype(np.float64) * u.ct
+                print('Sky=True')
+            else:
+
+                lamb = hdus[7].data[order].astype(np.float64) * u.AA
+                flux = hdus[1].data[order].astype(np.float64) * u.ct
+                unc =  hdus[4].data[order].astype(np.float64) * u.ct
+                print('Sky=False')
+
+            meta_dict = {
+                "x_values": np.arange(0, 2048, 1, dtype=np.int),
+                "m": grating_order,  # "header": hdr,
+            }
+            
+            uncertainty = StdDevUncertainty(unc)
+            mask = np.isnan(flux) | np.isnan(uncertainty.array)
+
+
+            super().__init__(
+                spectral_axis=lamb,
+                flux=flux,
+                mask=mask,
+                wcs=WCS(hdr),
+                uncertainty=uncertainty,
+                meta=meta_dict,
+                **kwargs
+            )
+        else:
+            super().__init__(*args, **kwargs)
+
+    def normalize(self):
+        """Normalize spectrum by its median value
+
+        Returns
+        -------
+        normalized_spec : (IGRINSSpectrum)
+            Normalized Spectrum
+        """
+        median_flux = np.nanmedian(self.flux)
+        #median_sky = np.nanmedian(self.sky)
+
+        return self.divide(median_flux, handle_meta="first_found")
+
+
+    def sky_subtract(self,sky):
+        """subtract science spectrum from sky spectrum
+
+        Returns
+        -------
+        sky_subtractedSpec : (IGRINSSpectrum)
+            Sky subtracted Spectrum
+        """
+        new_flux = self.flux-sky
+
+        return IGRINSSpectrum(
+            spectral_axis=self.wavelength,
+            flux=new_flux,
+        )
+
+    def blaze_subtract_spline(self):
+        """remove blaze function from spectrum by interpolating a spline function
+
+        Returns
+        -------
+        blaze corrrected spectrum : (IGRINSSpectrum)
+             
+        """
+        new_flux= self.normalize()
+
+        print(len(self.wavelength))
+
+        spline=UnivariateSpline(self.wavelength,new_flux.flux,k=5)
+        interp_spline= spline(self.wavelength) 
+
+        no_blaze=new_flux/interp_spline
+        return no_blaze
+
+    def blaze_subtract_flats(self, flat,order=19):
+        """remove blaze function from spectrum by subtracting by flat spectrum
+
+        Returns
+        -------
+        blaze corrrected spectrum using flat fields : (IGRINSSpectrum)
+             
+        """
+        new_flux= self.normalize()
+
+
+        flat_wv = flat[0]
+        flat_flux=flat[1]
+        if len(flat)==2:
+            flat_err=flat[2]
+
+        master_flat=flat_flux[order]/np.nanmedian(flat_flux[order])
+
+        flat_spline = InterpolatedUnivariateSpline(flat_wv[order],np.nan_to_num(master_flat), k=5)
+        interp_flat = flat_spline(self.wavelength)
+
+        no_flat=new_flux/interp_flat
+
+        return no_flat
+
+    def remove_nans(self):
+        """Remove data points that have NaN fluxes
+
+        Returns
+        -------
+        finite_spec : (IGRINSSpectrum)
+            Spectrum with NaNs removed
+        """
+        if self.uncertainty is not None:
+            masked_unc = StdDevUncertainty(self.uncertainty.array[~self.mask])
+        else:
+            masked_unc = None
+
+        meta_out = copy.deepcopy(self.meta)
+        meta_out["x_values"] = meta_out["x_values"][~self.mask]
+
+        return IGRINSSpectrum(
+            spectral_axis=self.wavelength[~self.mask],
+            flux=self.flux[~self.mask],
+            mask=self.mask[~self.mask],
+            uncertainty=masked_unc,
+            meta=meta_out,
+        )
+
+    def smooth_spectrum(self):
+        """Smooth the spectrum using Gaussian Process regression
+
+        Returns
+        -------
+        smoothed_spec : (IGRINSSpectrum)
+            Smooth version of input Spectrum
+        """
+        if self.uncertainty is not None:
+            unc = self.uncertainty.array
+        else:
+            unc = np.repeat(np.nanmedian(self.flux.value) / 100.0, len(self.flux))
+
+        kernel = terms.SHOTerm(sigma=0.03, rho=15.0, Q=0.5)
+        gp = celerite2.GaussianProcess(kernel, mean=0.0)
+        gp.compute(self.wavelength)
+
+        # Construct the GP model with celerite
+        def set_params(params, gp):
+            gp.mean = params[0]
+            theta = np.exp(params[1:])
+            gp.kernel = terms.SHOTerm(sigma=theta[0], rho=theta[1], Q=0.5)
+            gp.compute(self.wavelength.value, yerr=unc + theta[2], quiet=True)
+            return gp
+
+        def neg_log_like(params, gp):
+            gp = set_params(params, gp)
+            return -gp.log_likelihood(self.flux.value)
+
+        initial_params = [np.log(1), np.log(0.001), np.log(5.0), np.log(0.01)]
+        soln = minimize(neg_log_like, initial_params, method="L-BFGS-B", args=(gp,))
+        opt_gp = set_params(soln.x, gp)
+
+        mean_model = opt_gp.predict(self.flux.value, t=self.wavelength.value)
+
+        meta_out = copy.deepcopy(self.meta)
+        meta_out["x_values"] = meta_out["x_values"][~self.mask]
+
+        return IGRINSSpectrum(
+            spectral_axis=self.wavelength,
+            flux=mean_model * self.flux.unit,
+            mask=np.zeros_like(mean_model, dtype=np.bool),
+            meta=meta_out,
+        )
+
+    def plot(self, ax=None, ylo=0.6, yhi=1.2, figsize=(10, 4), **kwargs):
+        """Plot a quick look of the spectrum"
+
+        Parameters
+        ----------
+        ax : `~matplotlib.axes.Axes`
+            A matplotlib axes object to plot into. If no axes is provided,
+            a new one will be generated.
+        ylo : scalar
+            Lower limit of the y axis
+        yhi : scalar
+            Upper limit of the y axis
+        figsize : tuple
+            The figure size for the plot
+        label : str
+            The legend label to for plt.legend()
+
+        Returns
+        -------
+        ax : (`~matplotlib.axes.Axes`)
+            The axis to display and/or modify
+        """
+        if ax is None:
+            fig, ax = plt.subplots(1, figsize=figsize)
+            ax.set_ylim(ylo, yhi)
+            ax.set_xlabel("$\lambda \;(\AA)$")
+            ax.set_ylabel("Flux")
+            ax.step(self.wavelength, self.flux, **kwargs)
+        else:
+            ax.step(self.wavelength, self.flux, **kwargs)
+
+        return ax
+
+    def remove_outliers(self, threshold=5):
+        """Remove outliers above threshold
+
+        Parameters
+        ----------
+        threshold : float
+            The sigma-clipping threshold (in units of sigma)
+
+
+        Returns
+        -------
+        clean_spec : (IGRINSSpectrum)
+            Cleaned version of input Spectrum
+        """
+        residual = self.flux - self.smooth_spectrum().flux
+        mad = median_abs_deviation(residual.value)
+        mask = np.abs(residual.value) > threshold * mad
+
+        spectrum_out = copy.deepcopy(self)
+        spectrum_out._mask = mask
+        spectrum_out.flux[mask] = np.NaN
+
+        return spectrum_out.remove_nans()
+
+    def trim_edges(self, limits=(450, 1950)):
+        """Trim the order edges, which tend to be noisy
+
+        This method applies limits on absolute x pixel values, regardless
+        of the order of previous destructive operations, which may not
+        be the intended behavior in some applications.
+
+        Parameters
+        ----------
+        limits : tuple
+            The index bounds (lo, hi) for trimming the order
+
+        Returns
+        -------
+        trimmed_spec : (IGRINSSpectrum)
+            Trimmed version of input Spectrum
+        """
+        lo, hi = limits
+        meta_out = copy.deepcopy(self.meta)
+        x_values = meta_out["x_values"]
+        mask = (x_values < lo) | (x_values > hi)
+
+        if self.uncertainty is not None:
+            masked_unc = StdDevUncertainty(self.uncertainty.array[~mask])
+        else:
+            masked_unc = None
+
+        meta_out["x_values"] = x_values[~mask]
+
+        return IGRINSSpectrum(
+            spectral_axis=self.wavelength[~mask],
+            flux=self.flux[~mask],
+            mask=self.mask[~mask],
+            uncertainty=masked_unc,
+            meta=meta_out,
+        )
+
+    def estimate_uncertainty(self):
+        """Estimate the uncertainty based on residual after smoothing
+
+
+        Returns
+        -------
+        uncertainty : (np.float)
+            Typical uncertainty
+        """
+        residual = self.flux - self.smooth_spectrum().flux
+        return median_abs_deviation(residual.value)
+
+    def to_HDF5(self, path, file_basename):
+        """Export to spectral order to HDF5 file format
+        This format is required for IGRINS per-order Starfish input
+
+        Parameters
+        ----------
+        path : str
+            The directory destination for the HDF5 file
+        file_basename : str
+            The basename of the file to which the order number and extension
+            are appended.  Typically source name that matches a database entry.
+        """
+        grating_order = self.meta["m"]
+        out_path = path + "/" + file_basename + "_m{:03d}.hdf5".format(grating_order)
+
+        # The mask should be ones everywhere
+        mask_out = np.ones(len(self.wavelength), dtype=int)
+        f_new = h5py.File(out_path, "w")
+        f_new.create_dataset("fls", data=self.flux.value)
+        f_new.create_dataset("wls", data=self.wavelength.to(u.Angstrom).value)
+        f_new.create_dataset("sigmas", data=self.uncertainty.array)
+        f_new.create_dataset("masks", data=mask_out)
+        f_new.close()
+
+
+class IGRINSSpectrumList(SpectrumList):
+    r"""
+    An enhanced container for a list of IGRINS spectral orders
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def read(file, precache_hdus=True):
+        """Read in a SpectrumList from a file
+
+        Parameters
+        ----------
+        file : (str)
+            A path to a reduced IGRINS spectrum from plp
+        """
+       # assert '.spec_a0v.fits' in file
+        assert '.spectra.fits' in file
+
+        hdus = fits.open(file, memmap=False)
+        #sn_file = file[:-12] + "sn.fits"
+        #sn_hdus = fits.open(sn_file, memmap=False)
+        cached_hdus = [hdus]#, sn_hdus]
+
+        n_orders, n_pix = hdus[7].data.shape
+
+        list_out = []
+        for i in range(n_orders):
+            spec = IGRINSSpectrum(file=file, order=i, cached_hdus=cached_hdus)
+            list_out.append(spec)
+        return IGRINSSpectrumList(list_out)
+
+    def normalize(self):
+        """Normalize the all spectra to order 14's median
+        """
+        median_flux = copy.deepcopy(np.nanmedian(self[14].flux))
+        for i in range(len(self)):
+            self[i] = self[i].divide(median_flux, handle_meta="first_found")
+
+        return self
+    # def sky_subtract(self):
+    #     """Sky subtract all orders
+    #     """
+    #     flux = copy.deepcopy(self.flux)
+    #     sky = copy.deepcopy(self.sky)
+    #     for i in range(len(self)):
+    #         self[i] = flux[i] - sky[i]
+
+    #     return self
+
+    def remove_nans(self):
+        """Remove all the NaNs
+        """
+        for i in range(len(self)):
+            self[i] = self[i].remove_nans()
+
+        return self
+
+    def remove_outliers(self, threshold=5):
+        """Remove all the outliers
+
+        Parameters
+        ----------
+        threshold : float
+            The sigma-clipping threshold (in units of sigma)
+        """
+        for i in range(len(self)):
+            self[i] = self[i].remove_outliers(threshold=threshold)
+
+        return self
+
+    def trim_edges(self):
+        """Trim all the edges
+        """
+        for i in range(len(self)):
+            self[i] = self[i].trim_edges()
+
+        return self
+
+    def to_HDF5(self, path, file_basename):
+        """Save all spectral orders to the HDF5 file format
+        """
+        for i in range(len(self)):
+            self[i].to_HDF5(path, file_basename)
+
+    def plot(self, **kwargs):
+        """Plot the entire spectrum list
+        """
+        ax = self[0].plot(figsize=(25, 4), **kwargs)
+        for i in range(1, len(self)):
+            self[i].plot(ax=ax, **kwargs)
+
+        return ax
