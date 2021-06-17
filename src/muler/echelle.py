3727e8c9a52287b02676d05dd8de56725415d212
gully <igully@gmail.com>
2021-06-17 12:41:22 -0500

    :recycle: major movement of code from specific to general

--- src/muler/echelle.py
+++ src/muler/echelle.py
@@ -216,3 +216,144 @@ class EchelleSpectrum(Spectrum1D):
         #    new_self.meta["lfc"] = new_lfc
 
         return new_self
+
+    def smooth_spectrum(self):
+        """Smooth the spectrum using Gaussian Process regression
+
+        Returns
+        -------
+        smoothed_spec : (EchelleSpectrum)
+            Smooth version of input Spectrum
+        """
+        if self.uncertainty is not None:
+            unc = self.uncertainty.array
+        else:
+            unc = np.repeat(np.nanmedian(self.flux.value) / 100.0, len(self.flux))
+
+        kernel = terms.SHOTerm(sigma=0.03, rho=15.0, Q=0.5)
+        gp = celerite2.GaussianProcess(kernel, mean=0.0)
+        gp.compute(self.wavelength)
+
+        # Construct the GP model with celerite
+        def set_params(params, gp):
+            gp.mean = params[0]
+            theta = np.exp(params[1:])
+            gp.kernel = terms.SHOTerm(sigma=theta[0], rho=theta[1], Q=0.5)
+            gp.compute(self.wavelength.value, yerr=unc + theta[2], quiet=True)
+            return gp
+
+        def neg_log_like(params, gp):
+            gp = set_params(params, gp)
+            return -gp.log_likelihood(self.flux.value)
+
+        initial_params = [np.log(1), np.log(0.001), np.log(5.0), np.log(0.01)]
+        soln = minimize(neg_log_like, initial_params, method="L-BFGS-B", args=(gp,))
+        opt_gp = set_params(soln.x, gp)
+
+        mean_model = opt_gp.predict(self.flux.value, t=self.wavelength.value)
+
+        meta_out = copy.deepcopy(self.meta)
+        meta_out["x_values"] = meta_out["x_values"][~self.mask]
+
+        return self._copy(
+            spectral_axis=self.wavelength,
+            flux=mean_model * self.flux.unit,
+            mask=np.zeros_like(mean_model, dtype=np.bool),
+            meta=meta_out,
+        )
+
+    def plot(self, ax=None, ylo=0.6, yhi=1.2, figsize=(10, 4), **kwargs):
+        """Plot a quick look of the spectrum"
+
+        Parameters
+        ----------
+        ax : `~matplotlib.axes.Axes`
+            A matplotlib axes object to plot into. If no axes is provided,
+            a new one will be generated.
+        ylo : scalar
+            Lower limit of the y axis
+        yhi : scalar
+            Upper limit of the y axis
+        figsize : tuple
+            The figure size for the plot
+        label : str
+            The legend label to for plt.legend()
+
+        Returns
+        -------
+        ax : (`~matplotlib.axes.Axes`)
+            The axis to display and/or modify
+        """
+        if ax is None:
+            fig, ax = plt.subplots(1, figsize=figsize)
+            ax.set_ylim(ylo, yhi)
+            ax.set_xlabel("$\lambda \;(\AA)$")
+            ax.set_ylabel("Flux")
+            ax.step(self.wavelength, self.flux, **kwargs)
+        else:
+            ax.step(self.wavelength, self.flux, **kwargs)
+
+        return ax
+
+    def remove_outliers(self, threshold=5):
+        """Remove outliers above threshold
+
+        Parameters
+        ----------
+        threshold : float
+            The sigma-clipping threshold (in units of sigma)
+
+
+        Returns
+        -------
+        clean_spec : (KeckNIRSPECSpectrum)
+            Cleaned version of input Spectrum
+        """
+        residual = self.flux - self.smooth_spectrum().flux
+        mad = median_abs_deviation(residual.value)
+        mask = np.abs(residual.value) > threshold * mad
+
+        spectrum_out = copy.deepcopy(self)
+        spectrum_out._mask = mask
+        spectrum_out.flux[mask] = np.NaN
+
+        return spectrum_out.remove_nans()
+
+    def trim_edges(self, limits=None):
+        """Trim the order edges, which falloff in SNR
+
+        This method applies limits on absolute x pixel values, regardless
+        of the order of previous destructive operations, which may not
+        be the intended behavior in some applications.
+
+        Parameters
+        ----------
+        limits : tuple
+            The index bounds (lo, hi) for trimming the order
+
+        Returns
+        -------
+        trimmed_spec : (EchelleSpectrum)
+            Trimmed version of input Spectrum
+        """
+        if limits is None:
+            limits = self.noisy_edges
+        lo, hi = limits
+        meta_out = copy.deepcopy(self.meta)
+        x_values = meta_out["x_values"]
+        mask = (x_values < lo) | (x_values > hi)
+
+        if self.uncertainty is not None:
+            masked_unc = StdDevUncertainty(self.uncertainty.array[~mask])
+        else:
+            masked_unc = None
+
+        meta_out["x_values"] = x_values[~mask]
+
+        return self._copy(
+            spectral_axis=self.wavelength[~mask],
+            flux=self.flux[~mask],
+            mask=self.mask[~mask],
+            uncertainty=masked_unc,
+            meta=meta_out,
+        )
