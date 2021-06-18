e8597d700e48ed769471b0d09308fdd56d0df0cc
gully <igully@gmail.com>
2021-06-18 06:24:43 -0500

    :white_check_mark: tests pass after the :recycle: refactor

--- src/muler/igrins.py
+++ src/muler/igrins.py
@@ -10,6 +10,7 @@ IGRINSSpectrum
 """
 
 import warnings
+from astropy.time import Time
 import numpy as np
 import astropy
 from astropy.io import fits
@@ -93,7 +94,8 @@ class IGRINSSpectrum(EchelleSpectrum):
             flux = hdus["SPEC_DIVIDE_A0V"].data[order].astype(np.float64) * u.ct
             meta_dict = {
                 "x_values": np.arange(0, 2048, 1, dtype=np.int),
-                "m": grating_order,  # "header": hdr,
+                "m": grating_order,
+                "header": hdr,
             }
             if sn_hdus is not None:
                 sn = sn_hdus[0].data[10]
@@ -105,7 +107,7 @@ class IGRINSSpectrum(EchelleSpectrum):
                 mask = np.isnan(flux)
 
             super().__init__(
-                spectral_axis=lamb,
+                spectral_axis=lamb.to(u.Angstrom),
                 flux=flux,
                 mask=mask,
                 wcs=WCS(hdr),
@@ -116,180 +118,21 @@ class IGRINSSpectrum(EchelleSpectrum):
         else:
             super().__init__(*args, **kwargs)
 
-    def smooth_spectrum(self):
-        """Smooth the spectrum using Gaussian Process regression
+    @property
+    def RA(self):
+        """The right ascension from header files"""
+        return self.meta["header"]["OBJRA"] * u.deg
 
-        Returns
-        -------
-        smoothed_spec : (IGRINSSpectrum)
-            Smooth version of input Spectrum
-        """
-        if self.uncertainty is not None:
-            unc = self.uncertainty.array
-        else:
-            unc = np.repeat(np.nanmedian(self.flux.value) / 100.0, len(self.flux))
+    @property
+    def DEC(self):
+        """The declination from header files"""
+        return self.meta["header"]["OBJDEC"] * u.deg
 
-        kernel = terms.SHOTerm(sigma=0.03, rho=15.0, Q=0.5)
-        gp = celerite2.GaussianProcess(kernel, mean=0.0)
-        gp.compute(self.wavelength)
-
-        # Construct the GP model with celerite
-        def set_params(params, gp):
-            gp.mean = params[0]
-            theta = np.exp(params[1:])
-            gp.kernel = terms.SHOTerm(sigma=theta[0], rho=theta[1], Q=0.5)
-            gp.compute(self.wavelength.value, yerr=unc + theta[2], quiet=True)
-            return gp
-
-        def neg_log_like(params, gp):
-            gp = set_params(params, gp)
-            return -gp.log_likelihood(self.flux.value)
-
-        initial_params = [np.log(1), np.log(0.001), np.log(5.0), np.log(0.01)]
-        soln = minimize(neg_log_like, initial_params, method="L-BFGS-B", args=(gp,))
-        opt_gp = set_params(soln.x, gp)
-
-        mean_model = opt_gp.predict(self.flux.value, t=self.wavelength.value)
-
-        meta_out = copy.deepcopy(self.meta)
-        meta_out["x_values"] = meta_out["x_values"][~self.mask]
-
-        return IGRINSSpectrum(
-            spectral_axis=self.wavelength,
-            flux=mean_model * self.flux.unit,
-            mask=np.zeros_like(mean_model, dtype=np.bool),
-            meta=meta_out,
-        )
-
-    def plot(self, ax=None, ylo=0.6, yhi=1.2, figsize=(10, 4), **kwargs):
-        """Plot a quick look of the spectrum"
-
-        Parameters
-        ----------
-        ax : `~matplotlib.axes.Axes`
-            A matplotlib axes object to plot into. If no axes is provided,
-            a new one will be generated.
-        ylo : scalar
-            Lower limit of the y axis
-        yhi : scalar
-            Upper limit of the y axis
-        figsize : tuple
-            The figure size for the plot
-        label : str
-            The legend label to for plt.legend()
-
-        Returns
-        -------
-        ax : (`~matplotlib.axes.Axes`)
-            The axis to display and/or modify
-        """
-        if ax is None:
-            fig, ax = plt.subplots(1, figsize=figsize)
-            ax.set_ylim(ylo, yhi)
-            ax.set_xlabel("$\lambda \;(\AA)$")
-            ax.set_ylabel("Flux")
-            ax.step(self.wavelength, self.flux, **kwargs)
-        else:
-            ax.step(self.wavelength, self.flux, **kwargs)
-
-        return ax
-
-    def remove_outliers(self, threshold=5):
-        """Remove outliers above threshold
-
-        Parameters
-        ----------
-        threshold : float
-            The sigma-clipping threshold (in units of sigma)
-
-
-        Returns
-        -------
-        clean_spec : (IGRINSSpectrum)
-            Cleaned version of input Spectrum
-        """
-        residual = self.flux - self.smooth_spectrum().flux
-        mad = median_abs_deviation(residual.value)
-        mask = np.abs(residual.value) > threshold * mad
-
-        spectrum_out = copy.deepcopy(self)
-        spectrum_out._mask = mask
-        spectrum_out.flux[mask] = np.NaN
-
-        return spectrum_out.remove_nans()
-
-    def trim_edges(self, limits=(450, 1950)):
-        """Trim the order edges, which tend to be noisy
-
-        This method applies limits on absolute x pixel values, regardless
-        of the order of previous destructive operations, which may not
-        be the intended behavior in some applications.
-
-        Parameters
-        ----------
-        limits : tuple
-            The index bounds (lo, hi) for trimming the order
-
-        Returns
-        -------
-        trimmed_spec : (IGRINSSpectrum)
-            Trimmed version of input Spectrum
-        """
-        lo, hi = limits
-        meta_out = copy.deepcopy(self.meta)
-        x_values = meta_out["x_values"]
-        mask = (x_values < lo) | (x_values > hi)
-
-        if self.uncertainty is not None:
-            masked_unc = StdDevUncertainty(self.uncertainty.array[~mask])
-        else:
-            masked_unc = None
-
-        meta_out["x_values"] = x_values[~mask]
-
-        return IGRINSSpectrum(
-            spectral_axis=self.wavelength[~mask],
-            flux=self.flux[~mask],
-            mask=self.mask[~mask],
-            uncertainty=masked_unc,
-            meta=meta_out,
-        )
-
-    def estimate_uncertainty(self):
-        """Estimate the uncertainty based on residual after smoothing
-
-
-        Returns
-        -------
-        uncertainty : (np.float)
-            Typical uncertainty
-        """
-        residual = self.flux - self.smooth_spectrum().flux
-        return median_abs_deviation(residual.value)
-
-    def to_HDF5(self, path, file_basename):
-        """Export to spectral order to HDF5 file format
-        This format is required for IGRINS per-order Starfish input
-
-        Parameters
-        ----------
-        path : str
-            The directory destination for the HDF5 file
-        file_basename : str
-            The basename of the file to which the order number and extension
-            are appended.  Typically source name that matches a database entry.
-        """
-        grating_order = self.meta["m"]
-        out_path = path + "/" + file_basename + "_m{:03d}.hdf5".format(grating_order)
-
-        # The mask should be ones everywhere
-        mask_out = np.ones(len(self.wavelength), dtype=int)
-        f_new = h5py.File(out_path, "w")
-        f_new.create_dataset("fls", data=self.flux.value)
-        f_new.create_dataset("wls", data=self.wavelength.to(u.Angstrom).value)
-        f_new.create_dataset("sigmas", data=self.uncertainty.array)
-        f_new.create_dataset("masks", data=mask_out)
-        f_new.close()
+    @property
+    def astropy_time(self):
+        """The astropy time based on the header"""
+        mjd = self.meta["header"]["MJD-OBS"]
+        return Time(mjd, format="mjd", scale="utc")
 
 
 class IGRINSSpectrumList(SpectrumList):
--- tests/test_igrins.py
+++ tests/test_igrins.py
@@ -87,7 +87,8 @@ def test_equivalent_width():
     assert equivalent_width is not None
     assert type(equivalent_width) is not int
     assert type(equivalent_width) is astropy.units.quantity.Quantity
-    assert equivalent_width.unit is spec.wavelength.unit
+    new_unit = equivalent_width.to(spec.wavelength.unit)
+    assert new_unit.unit == spec.wavelength.unit
 
 
 def test_smoothing():
