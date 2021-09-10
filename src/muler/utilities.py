140a3c6232f3e3acafdfb94666757fa5a152fa88
gully <igully@gmail.com>
2021-09-10 15:47:53 -0500

    Combine function testing (#44)
    * Add imports for the utilities
    
    * Add a unit test for the combine funciton
    
    * :test_tube: Add a failing test
    
    * :white_check_mark: Tests pass after tweak to the combine method!
    
    * rename the combine function to present tense: combine instead of combined

--- src/muler/utilities.py
+++ src/muler/utilities.py
@@ -1,8 +1,10 @@
-def combined_spectra(spec_list):
+import numpy as np
+from astropy.nddata.nduncertainty import StdDevUncertainty
+
+
+def combine_spectra(spec_list):
     spec_final = spec_list[0]
-    for i in range(len(spec_list)-1):
-        if i != len(spec_list)-1: 
-            new_sigma = np.sqrt(1/spec_list[i+1].uncertainty.array) 
-            spec_list[i+1].uncertainty = StdDevUncertainty(new_sigma)
-            spec_final = spec_final.add(spec_list[i+1], propagate_uncertainties=True)
-    return (spec_final)
+    for i in range(1, len(spec_list)):
+        spec_final = spec_final.add(spec_list[i], propagate_uncertainties=True)
+    return spec_final
+
---
+++ tests/test_utilities.py
@@ -0,0 +1,79 @@
+import time
+from muler.hpf import HPFSpectrum
+from specutils import Spectrum1D
+import glob
+from astropy.nddata.nduncertainty import StdDevUncertainty
+import numpy as np
+
+from muler.utilities import combine_spectra
+
+# There should be exactly 3 files in the example data directory
+local_files = glob.glob("**/01_A0V_standards/Goldilocks_*.spectra.fits", recursive=True)
+
+
+def test_combine():
+    """Does the combine utility work?"""
+    n_files = len(local_files)
+    this_spec_list = [
+        HPFSpectrum(file=local_files[i], order=10) for i in range(n_files)
+    ]
+
+    assert len(this_spec_list) == n_files
+    assert n_files == 3
+    assert this_spec_list is not None
+
+    coadded_spectrum = combine_spectra(this_spec_list)
+
+    assert isinstance(coadded_spectrum, Spectrum1D)
+    assert isinstance(coadded_spectrum, HPFSpectrum)
+    assert len(coadded_spectrum.flux) == len(coadded_spectrum.uncertainty.array)
+
+    ## Does the signal-to-noise ratio scale the way we'd expect?
+    # Make a single fake HPF spectrum
+    reference_spectrum = this_spec_list[0]
+    n_pixels = len(reference_spectrum.wavelength)
+
+    # SNR = 100
+    input_sigma = 0.01
+    input_mean = 1.0
+    input_snr = input_mean / input_sigma
+    uncertainty_vector = np.repeat(input_sigma, repeats=n_pixels)
+    flux_vector = np.ones_like(uncertainty_vector)
+
+    fake_spectrum = HPFSpectrum(
+        spectral_axis=reference_spectrum.wavelength,
+        flux=flux_vector * reference_spectrum.flux.unit,
+        uncertainty=StdDevUncertainty(uncertainty_vector),
+    )
+
+    assert isinstance(fake_spectrum, HPFSpectrum)
+
+    # Combining 4 spectra should double the SNR: sqrt(4) = 2
+    fake_spectrum_list = [fake_spectrum] * 4
+    net_spectrum = combine_spectra(fake_spectrum_list)
+
+    net_snr = np.mean(net_spectrum.flux.value / net_spectrum.uncertainty.array)
+    assert net_snr == (input_snr * 2)
+
+    # Test combining several copies of that spectrum
+    n_spectra_tests = [9, 16, 25, 36, 100, 1000, 2, 1]
+
+    for n_spectra in n_spectra_tests:
+        fake_spectrum_list = [fake_spectrum] * n_spectra
+
+        t0 = time.time()
+        net_spectrum = combine_spectra(fake_spectrum_list)
+        t1 = time.time()
+        net_time = t1 - t0
+        print(
+            f"\n\t Net time for combining {n_spectra} spectra: {net_time:0.5f} seconds",
+            end="\t",
+        )
+
+        # The new SNR should be twice as low as before:
+        assert np.allclose(
+            np.mean(fake_spectrum.uncertainty.array) * np.sqrt(n_spectra),
+            np.mean(net_spectrum.uncertainty.array),
+        ), "Coadding {} spectra should root-N down the uncertainty to {}".format(
+            n_spectra, np.sqrt(n_spectra)
+        )
