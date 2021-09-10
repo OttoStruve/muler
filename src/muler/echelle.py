f24037ae2a780b71d86ee58e4f458719ac6b58c2
kfkaplan <triclon@gmail.com>
2021-09-10 10:17:15 -0700

    Alternate IGRINS spectra input and some additions to EchelleSpectrumList (#39)
    * Added ability to read in IGRINS .spec.fits files.  When this is done, the user will need to specify a .wave.fits filie storing the wavelength solution.
    
    * Added band math and deblazing to EchelleSpectrumList.  Also fixed minor bug where copy.copy should have been copy.deepcopy.
    
    * Fixed some typo errors in deblaze description.
    
    * Fixed some things with the IGRINS input.
    
    * A few fixes to igrins.py.
    
    * One more fix to igrins.py
    
    * Modified EchelleSpectrumList plotting to better handle ax being passed to it.
    
    * Fixed issue with logger not being imported by igrins.py

--- src/muler/echelle.py
+++ src/muler/echelle.py
@@ -150,7 +150,7 @@ class EchelleSpectrum(Spectrum1D):
     def deblaze(self, method="spline"):
         """Remove blaze function from spectrum by interpolating a spline function
 
-        Note: It is recommended to remove NaNs before running this operation,
+                Note: It is recommended to remove NaNs before running this operation,
                 otherwise edge effects can be appear from zero-padded edges.
 
         Returns
@@ -474,9 +474,21 @@ class EchelleSpectrumList(SpectrumList):
 
         return self
 
+    def deblaze(self, method="spline"):
+        """Remove blaze function from all orders by interpolating a spline function
+
+        Note: It is recommended to remove NaNs before running this operation,
+                otherwise  effects can be appear from zero-padded edges.
+        """
+        spec_out = copy.deepcopy(self)
+        for i in range(len(self)):
+            spec_out[i] = self[i].deblaze(method=method)
+        return spec_out
+
+
     def flatten_by_black_body(self, Teff):
         """Flatten by black body"""
-        spec_out = copy.copy(self)
+        spec_out = copy.deepcopy(self)
         index = self.normalization_order_index
         median_wl = copy.deepcopy(np.nanmedian(self[index].wavelength))
 
@@ -517,8 +529,45 @@ class EchelleSpectrumList(SpectrumList):
     def plot(self, **kwargs):
         """Plot the entire spectrum list
         """
-        ax = self[0].plot(figsize=(25, 4), **kwargs)
-        for i in range(1, len(self)):
-            self[i].plot(ax=ax, **kwargs)
+        if not "ax" in kwargs:
+            ax = self[0].plot(figsize=(25, 4), **kwargs)
+            for i in range(1, len(self)):
+                self[i].plot(ax=ax, **kwargs)
+            return ax
+        else:
+            for i in range(1, len(self)):
+                self[i].plot(**kwargs)
 
-        return ax
+        
+
+    def __add__(self, other):
+        """Bandmath addition
+        """
+        spec_out = copy.deepcopy(self)
+        for i in range(len(self)):
+            spec_out[i] = self[i] + other[i]
+        return spec_out
+
+    def __sub__(self, other):
+        """Bandmath subtraction
+        """
+        spec_out = copy.deepcopy(self)
+        for i in range(len(self)):
+            spec_out[i] = self[i] - other[i]
+        return spec_out
+
+    def __mul__(self, other):
+        """Bandmath multiplication
+        """
+        spec_out = copy.deepcopy(self)
+        for i in range(len(self)):
+            spec_out[i] = self[i] * other[i]
+        return spec_out
+
+    def __truediv__(self, other):
+        """Bandmath division
+        """
+        spec_out = copy.deepcopy(self)
+        for i in range(len(self)):
+            spec_out[i] = self[i] / other[i]
+        return spec_out
--- src/muler/igrins.py
+++ src/muler/igrins.py
@@ -8,7 +8,7 @@ A container for an IGRINS spectrum of :math:`M=28` total total orders :math:`m`,
 IGRINSSpectrum
 ##############
 """
-
+import logging
 import warnings
 from muler.echelle import EchelleSpectrum, EchelleSpectrumList
 from astropy.time import Time
@@ -21,6 +21,7 @@ from astropy.nddata import StdDevUncertainty
 
 import copy
 
+log = logging.getLogger(__name__)
 
 #  See Issue: https://github.com/astropy/specutils/issues/779
 warnings.filterwarnings(
@@ -42,21 +43,26 @@ class IGRINSSpectrum(EchelleSpectrum):
     A container for IGRINS spectra
 
     Args:
-        file (str): A path to a reduced IGRINS spectrum from plp
+        file (str): A path to a reduced IGRINS spectrum from plp of file type .spec.fits
+            or .spec_a0v.fits.
         order (int): which spectral order to read
         cached_hdus (list) :
-            List of two fits HDUs, one for the spec_a0v.fits, and one for the
-            sn.fits file, to reduce file I/O for multiorder access.
-            If provided, must give both HDUs.  Optional, default is None.
+            List of two or three fits HDUs, one for the spec.fits/spec_a0v.fits, one for the
+            sn.fits file, and one optional one for the .wave.fits file
+            to reduce file I/O for multiorder access.
+            If provided, must give both (or three) HDUs.  Optional, default is None.
+        wavefile (str):  A path to a reduced IGRINS spectrum storing the wavelength solution
+            of file type .wave.fits.
     """
 
-    def __init__(self, *args, file=None, order=10, cached_hdus=None, **kwargs):
+    def __init__(self, *args, file=None, order=10, cached_hdus=None, wavefile=None, **kwargs):
 
         self.ancillary_spectra = None
         self.noisy_edges = (450, 1950)
         self.instrumental_resolution = 45_000.0
 
         if file is not None:
+            assert (".spec_a0v.fits" in file) or (".spec.fits" in file)
             # Determine the band
             if "SDCH" in file:
                 band = "H"
@@ -66,19 +72,36 @@ class IGRINSSpectrum(EchelleSpectrum):
                 raise NameError("Cannot identify file as an IGRINS spectrum")
             grating_order = grating_order_offsets[band] + order
 
-            sn_file = file[:-13] + "sn.fits"
+            if ".spec_a0v.fits" in file:
+                sn_file = file[:-13] + "sn.fits"
+            elif ".spec.fits" in file:
+                sn_file = file[:-9] + "sn.fits"
             if cached_hdus is not None:
                 hdus = cached_hdus[0]
                 sn_hdus = cached_hdus[1]
+                if wavefile is not None:
+                    wave_hdus = cached_hdus[2]
             else:
                 hdus = fits.open(str(file))
                 try:
                     sn_hdus = fits.open(sn_file)
                 except:
                     sn_hdus = None
+                if wavefile is not None:
+                    wave_hdus = fits.open(wavefile)
             hdr = hdus[0].header
-            lamb = hdus["WAVELENGTH"].data[order].astype(np.float64) * u.micron
-            flux = hdus["SPEC_DIVIDE_A0V"].data[order].astype(np.float64) * u.ct
+            if ("spec_a0v.fits" in file) and (wavefile is not None):
+                log.warn("You have passed in a wavefile and a spec_a0v format file, which has its own wavelength solution.  Ignoring the wavefile.")
+            elif ".spec_a0v.fits" in file:
+                lamb = hdus["WAVELENGTH"].data[order].astype(np.float64) * u.micron
+                flux = hdus["SPEC_DIVIDE_A0V"].data[order].astype(np.float64) * u.ct
+            elif ("spec.fits" in file) and (wavefile is not None):
+                lamb = wave_hdus[0].data[order].astype(np.float64) * 1e-3 * u.micron #Note .wave.fits and .wavesol_v1.fts files store their wavelenghts in nm so they need to be converted to microns
+                flux = hdus[0].data[order].astype(np.float64) * u.ct
+            elif ("spec.fits" in file) and (wavefile is None):
+                raise Exception("wavefile must be specified when passing in spec.fits files, which do not come with an in-built wavelength solution.")
+            else:
+                raise Exception("File "+file+" is the wrong file type.  It must be either .spec_a0v.fits or .spec.fits.")
             meta_dict = {
                 "x_values": np.arange(0, 2048, 1, dtype=np.int),
                 "m": grating_order,
@@ -140,24 +163,33 @@ class IGRINSSpectrumList(EchelleSpectrumList):
         super().__init__(*args, **kwargs)
 
     @staticmethod
-    def read(file, precache_hdus=True):
+    def read(file, precache_hdus=True, wavefile=None):
         """Read in a SpectrumList from a file
 
         Parameters
         ----------
         file : (str)
             A path to a reduced IGRINS spectrum from plp
+        wafeile : (str)
+
         """
-        assert ".spec_a0v.fits" in file
+        assert (".spec_a0v.fits" in file) or (".spec.fits" in file)
         hdus = fits.open(file, memmap=False)
-        sn_file = file[:-13] + "sn.fits"
+        if ".spec_a0v.fits" in file:
+            sn_file = file[:-13] + "sn.fits"
+        elif ".spec.fits" in file:
+            sn_file = file[:-9] + "sn.fits"
         sn_hdus = fits.open(sn_file, memmap=False)
         cached_hdus = [hdus, sn_hdus]
+        if wavefile is not None:
+            wave_hdus = fits.open(wavefile, memmap=False)
+            cached_hdus.append(wave_hdus)
 
-        n_orders, n_pix = hdus["WAVELENGTH"].data.shape
+        #n_orders, n_pix = hdus["WAVELENGTH"].data.shape
+        n_orders, n_pix = hdus[0].data.shape
 
         list_out = []
         for i in range(n_orders):
-            spec = IGRINSSpectrum(file=file, order=i, cached_hdus=cached_hdus)
+            spec = IGRINSSpectrum(file=file, wavefile=wavefile, order=i, cached_hdus=cached_hdus)
             list_out.append(spec)
         return IGRINSSpectrumList(list_out)
