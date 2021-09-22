51272d6111fe9c4aefab392730aa21a7f5745e6f
kkaplan <kkaplan@usra.edu>
2021-09-22 12:45:54 -0700

    Updated description of resample_list and moved to utilities.py.

--- src/muler/echelle.py
+++ src/muler/echelle.py
@@ -66,6 +66,17 @@ def resample_list(spec_to_resample, specList, **kwargs):
     """
     Resample a single EchelleSpectrum or Spectrum1D object into a EchelleSpectrumList object.
     Useful for converting models into echelle spectra with multiple orders.
+    
+    Parameters
+    ----------
+    spec_to_resample: EchelleSpectrum or specutils Spectrum1D object
+        Object storing spectrum (typically of a model) to be resampled onto the same grid as specList.
+    specList: EchelleSpectrumList object
+        Object storing an echelle spectrum (spectrum with multiple orders) with the wavelength grid to 
+        which spec_to_resample will be resampled.
+    **kwargs: optional
+        Extra arguments to be passed to specutils.manipulation.resample which is run to resample
+        spec_to_resample to each order in specList
     """
     spec_out = copy.deepcopy(specList)
     for i in range(len(specList)):
