9096f0bd073764e92fa55e9e028878534d9d8b04
kkaplan <kkaplan@usra.edu>
2021-09-22 13:26:17 -0700

    Adding resample_list() to utilities.

--- src/muler/utilities.py
+++ src/muler/utilities.py
@@ -8,3 +8,23 @@ def combine_spectra(spec_list):
         spec_final = spec_final.add(spec_list[i], propagate_uncertainties=True)
     return spec_final
 
+def resample_list(spec_to_resample, specList, **kwargs):
+    """
+    Resample a single EchelleSpectrum or Spectrum1D object into a EchelleSpectrumList object.
+    Useful for converting models into echelle spectra with multiple orders.
+    
+    Parameters
+    ----------
+    spec_to_resample: EchelleSpectrum or specutils Spectrum1D object
+        Object storing spectrum (typically of a model) to be resampled onto the same grid as specList.
+    specList: EchelleSpectrumList object
+        Object storing an echelle spectrum (spectrum with multiple orders) with the wavelength grid to 
+        which spec_to_resample will be resampled.
+    **kwargs: optional
+        Extra arguments to be passed to specutils.manipulation.resample which is run to resample
+        spec_to_resample to each order in specList
+    """
+    spec_out = copy.deepcopy(specList)
+    for i in range(len(specList)):
+        spec_out[i] = spec_to_resample.resample(specList[i], **kwargs)
+    return spec_out
