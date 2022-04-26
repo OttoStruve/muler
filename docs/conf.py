7e670e1292ee60707bc80ff1167b4611b6b45cd4
gully <igully@gmail.com>
2022-04-26 13:36:21 -0500

    Hmm, mimick what lightkurve does to get tutorials to compile

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,10 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../src/"))
+sys.path.append(os.path.join(os.path.dirname(__name__), ".."))
+
+import muler
+
 
 # -- Project information -----------------------------------------------------
 
