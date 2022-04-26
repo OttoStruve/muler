03476579e49f9b6700abfb6cafeee9cee6b63727
gully <igully@gmail.com>
2022-04-26 14:22:09 -0500

    RTD fails due to importlib resources friction...

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../src/"))
-sys.path.append(os.path.join(os.path.dirname(__name__), ".."))
+# sys.path.append(os.path.join(os.path.dirname(__name__), ".."))
 
 import muler
 
