f9aa8ae3e1f18cf5b8c37b89387ecb2a146bd4cd
kkaplan <kkaplan@usra.edu>
2022-10-26 16:39:38 -0700

    Fix mising variable.

--- src/muler/igrins.py
+++ src/muler/igrins.py
@@ -138,7 +138,7 @@ class IGRINSSpectrum(EchelleSpectrum):
             elif variance_hdus is None and ".spec_a0v.fits" in file:
                 s2n = fits.getdata
             else:
-                 = None
+                uncertainity = None
                 mask = np.isnan(flux)
 
             super().__init__(
